e6f2f28ab46049096bf8292f611d02f873f75d22
aihe <379879419@qq.com>
2019-06-03 19:48:34 +0800

    Update norvig_spell.py

--- evaluate/norvig_spell.py
+++ evaluate/norvig_spell.py
@@ -9,9 +9,12 @@ TOTAL_WORDS = 0
 def init(filename = 'big.txt'):
     global WORDS
     global TOTAL_WORDS
+    #统计词频，并存储为词典
     WORDS = Counter(words(open(filename).read()))
+    #统计总词数
     TOTAL_WORDS=sum(WORDS.values())
 
+#统计每一个词的词频占比
 def P(word, N=None):
     "Probability of `word`."
     N = N or TOTAL_WORDS
@@ -27,11 +30,11 @@ def correction(word):
     if cands[0] == word:
         return word
     return sorted(cands, key=P, reverse=True)
-
+#遍历每一个词并确保其在词典中
 def known(words):
     "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
-
+#编辑距离为1的单词
 def edits1(word):
     "All edits that are one edit away from `word`."
     letters    = 'abcdefghijklmnopqrstuvwxyz'
@@ -41,7 +44,7 @@ def edits1(word):
     replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
     inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
-
+#编辑距离为2的单词
 def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
